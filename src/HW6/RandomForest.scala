1a852b2b8c41722c177e05f044df5efde379520c
amaliujia <amaliujia@163.com>
2015-11-16 17:55:28 -0500

    Done?

--- src/HW6/RandomForest.scala
+++ src/HW6/RandomForest.scala
@@ -1,3 +1,6 @@
+import java.io.{FileWriter, BufferedWriter, File}
+
+import HW6.CassandraDriver
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.mllib.tree.RandomForest
 import org.apache.spark.mllib.tree.model.RandomForestModel
@@ -7,10 +10,17 @@ import org.apache.spark.mllib.util.MLUtils
 val conf = new SparkConf().setAppName("Spark_RandomForest").setMaster("local[1]")
 val sc = new SparkContext(conf)
 
+//
+val driver = CassandraDriver.getInstance()
+val file = new File("data/sample.txt")
+val bw = new BufferedWriter(new FileWriter(file))
+bw.write(driver.queryData("test", "min max avg label", 4).toString())
+bw.close()
+
 // Load and parse the data file.
 val data = MLUtils.loadLibSVMFile(sc, "data/sample.txt")
 // Split the data into training and test sets (30% held out for testing)
-val splits = data.randomSplit(Array(0.8, 0.1))
+val splits = data.randomSplit(Array(0.8, 0.2))
 val (trainingData, testData) = (splits(0), splits(1))
 
 // Train a RandomForest model.
@@ -26,6 +36,10 @@ val maxBins = 32
 val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
   numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
 
+// Save and load model
+model.save(sc, "myModelPath")
+val sameModel = RandomForestModel.load(sc, "myModelPath")
+
 // Evaluate model on test instances and compute test error
 val labelAndPreds = testData.map { point =>
   val prediction = model.predict(point.features)
@@ -35,6 +49,7 @@ val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.
 println("Test Error = " + testErr)
 println("Learned classification forest model:\n" + model.toDebugString)
 
-// Save and load model
-model.save(sc, "myModelPath")
-val sameModel = RandomForestModel.load(sc, "myModelPath")
+// write model back to Cassandra.
+driver.insertData(model.toString(), "model")
+// write test result back to Cassandra
+driver.insertData(testErr.toString(), "error")
